5e87fef4d6d20963c236c008fe5d9166c89158f1
MrBerkut <berk526549@gmail.com>
2020-02-20 23:04:44 +0500

    с окном ввода и удалением элементов

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function myFunc(){
-  let str = document.getElementById('spisok').value;
+  let str = document.getElementById('spisok').value; //получаем текст из окна ввода
   
   let arr = str.split('\n'); /* разделяем строку построчно, как бы это ни звучало */
   let list=[];
@@ -7,7 +7,7 @@ function myFunc(){
     let currentText = arr[i].substring(1) , /* полученная с помощью substring подстрока с текстом */
       currentMarker = arr[i].substring(0,1); /* полученная с помощью substring подстрока с маркером */
       if (currentMarker=='' || currentMarker==' ') {continue} /* убираем пустые строки */
-    switch(currentMarker) {
+    switch(currentMarker) { //по маркеру присваиваем тип
         case '>':
           currentType = 'code';
           break;
@@ -32,12 +32,27 @@ function myFunc(){
           type: currentType
       })
   }
-  for (let i = 0; i < list.length; i++ ){
-    var newElem = document.createElement('p'); /* создаем абзац */
+  for (let i = 0; i < list.length; i++ ){ 
+    let rng = Math.random().toString().substr(2) //создаем случайный id и отбрасываем 0. и получается целое число
+    let newElem = document.createElement('p'); /* создаем абзац */
     newElem.className = list[i].type; /* присваиваем абзацу тип */
-    let text = document.createTextNode(list[i].text); /* это я сам не понял зачем) объяснишь? */
-    newElem.appendChild(text);/* в абзац добавляем текст, взятый из массива */
-    document.body.appendChild(newElem); /* добавляем абзац в body */
+    let newButton = document.createElement('button'); //создаем кнопку для удаления
+    newButton.addEventListener( "click" , () => delEl(rng)); //по клику срабатывает функция delEl
+    newButton.className = 'del'; //даем кнопке class del
+    newButton.innerHTML = 'удалить текст над кнопкой'; //вставляем текст в нашу кнопку
+    newElem.innerHTML = list[i].text; //вставляем текст в newElem
+    let newDiv = document.createElement('div') //создаем dic, в котором будет текст и кнопка, чтобы потом его удалить
+    newDiv.id = rng; //выдаем айдишник диву
+    newDiv.appendChild(newElem); //запихиваем в див текст
+    newDiv.appendChild(newButton); //и кнопку
+    document.body.appendChild(newDiv); //див запихиваем в body
   }
-  document.getElementById('spisok').value ='';
+  document.getElementById('spisok').value =''; //очищаем окно ввода
   }
+document.myFunc = myFunc; //это для того, чтобы функция работала на localhost и может еще где-нибудь
+
+function delEl(delId){ //создаем функцию для удаления по кнопке
+  let element = document.getElementById(delId); //получаем наш div по id
+  element.remove(); //удаляем div
+}
+document.delEl = delEl;
